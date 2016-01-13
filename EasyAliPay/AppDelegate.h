c50a5dc85ab85ce6dea50eaa942d35d159980c4c
ileo <work.mileo@gmail.com>
2016-01-13 17:56:03 +0800

    init

---
+++ EasyAliPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyAliPay
+//
+//  Created by ileo on 16/1/13.
+//  Copyright © 2016年 ileo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyAliPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyAliPay
+//
+//  Created by ileo on 16/1/13.
+//  Copyright © 2016年 ileo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
