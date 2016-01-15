4611e8ae65ce08d02e3ba484f6c36542bd98bf3d
ileo <work.mileo@gmail.com>
2016-01-15 15:44:42 +0800

    添加注释

--- EasyAliPay.podspec
+++ EasyAliPay.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "EasyAliPay"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "简单封装ali支付"
   s.description  = <<-DESC
 					 简单封装ali支付
--- EasyAliPay/AliPay/EasyAliPay.h
+++ EasyAliPay/AliPay/EasyAliPay.h
@@ -11,8 +11,22 @@
 
 @interface EasyAliPay : NSObject
 
+
+/**
+ *  跳转支付宝支付
+ *
+ *  @param content  订单内容
+ *  @param callBack 支付回调
+ */
 +(void)payAliOrderContent:(AliOrderContent *)content callBack:(void (^)(BOOL success, NSDictionary *result))callBack;
 
+
+/**
+ *  AppDelegate 用于接收支付宝打开APP时的数据
+ *
+ *  @param url      回调URL
+ *  @param callBack 支付回调
+ */
 +(void)appOpenURL:(NSURL *)url callBack:(void (^)(BOOL success, NSDictionary *result))callBack;
 
 @end
